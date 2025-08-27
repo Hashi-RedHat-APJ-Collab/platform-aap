203c54f4abcbdcbbf13b78dfa3c0ff47a442fe0b
Aaron Evans <aarone@outlook.com.au>
2025-08-27 10:08:36 +1000

    fix: Increase unhealthy threshold for ALB target group health checks

--- aap_instance/alb.tf
+++ aap_instance/alb.tf
@@ -74,7 +74,7 @@ resource "aws_lb_target_group" "app_tg" {
     interval            = 30
     timeout             = 5
     healthy_threshold   = 5
-    unhealthy_threshold = 2
+    unhealthy_threshold = 4
     matcher             = "200-399"
   }
 }
